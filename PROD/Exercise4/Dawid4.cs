011123f9b9b38da5beb37ec40019a3d9b45d8b91
michciq <91256300+michciq@users.noreply.github.com>
2021-10-07 12:44:54 +0200

    UPDATE  Dawid4 - added Evo3

--- PROD/Exercise4/Dawid4.cs
+++ PROD/Exercise4/Dawid4.cs
@@ -83,4 +83,41 @@ namespace Exercise4
             return false;
         }
     }
+    
+    public class Dawid_Evo3 : Exercise
+    {
+        public int Run()
+        {
+            int first;
+            int second;
+            int product;
+            int currentHighest = 0;
+
+            for (first = 999; first > 99; first--)
+            {
+                for (second = 999; second > 99; second--)
+                {
+                    product = first * second;
+                    if (product > currentHighest)
+                        if (IsPalindrome(product))
+                            currentHighest = product;
+                }
+            }
+            return currentHighest;
+        }
+
+        private bool IsPalindrome(int number)
+        {
+            if (number >= 100000)
+                if ((number/100000) == number % 10)
+                    if ((number/10000) % 10 == (number/10) % 10)
+                        if ((number/1000) % 10 == (number/100) % 10)
+                            return true;
+            else if (number >= 10000)
+                if ((number/10000) == number % 10)
+                    if ((number/1000) % 10 == (number/10) % 10)
+                        return true;
+            return false;
+        }
+    }
 }
