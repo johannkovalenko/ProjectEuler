47acc47a3f4eaa012d0cc4fb62f94e2f28f1bba4
ueueue1970 <74244538+ueueue69@users.noreply.github.com>
2021-10-16 16:35:18 +0200

    Krystian adding exercise 6

---
+++ PROD/Exercise6/Krystian.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System;
+
+namespace Exercise4
+{
+    class Krystian : Exercise
+    {
+        public int Run()
+        {
+            int n = 100;
+            int sumOfSquares = 0;
+            int squareSum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sumOfSquares += i * i;
+                squareSum += i;
+            }
+            int difference = squareSum * squareSum - sumOfSquares;
+            return difference;
+
+        }
+    }
+}
